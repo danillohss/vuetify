3cf4d0e7dbfb95229693039b96f1f951fe479755
Danillo Soares <danillohss@gmail.com>
2024-04-22 21:05:35 -0300

    Vuetify - Init

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-app>
+    <v-app-bar class="border b" title="meuapp"></v-app-bar>
+    <v-main>
+      <v-container>
+        <div class="d-flex justify-space-between">
+          <div
+            style="width: 100px; height: 100px"
+            class="border rounded-lg elevation2"
+          >
+            TESTE
+          </div>
+          <div
+            style="width: 100px; height: 100px"
+            class="border rounded-lg elevation2"
+          >
+            TESTE
+          </div>
+          <div
+            style="width: 100px; height: 100px"
+            class="border rounded-lg elevation2"
+          >
+            TESTE
+          </div>
+        </div>
+      </v-container>
+      <HelloWorld />
+    </v-main>
+
+    <AppFooter />
+  </v-app>
+</template>
+
+<script setup>
+//
+</script>
